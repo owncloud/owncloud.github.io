faeae7f8a4dbff676071945731bd82c75f570e82
Michael Barz <mbarz@owncloud.com>
2020-12-22 16:31:39 +0000

    commit 8d9c53c6a9d1e0b136b5970275e10337a32d87d2 Merge: 8c635a8 78c86a3 Author: Michael Barz <mbarz@owncloud.com> Date:   Thu Dec 10 17:02:07 2020 +0100
        Merge pull request #21 from wkloucek/bump_geekdoc_version
    
        bump version of geekdoc

--- js/en.search.min.a198087940c1635bf1ef6ab183c8a15925569a2d81b010504a3ebde6f050a076.js
+++ js/en.search.min.a198087940c1635bf1ef6ab183c8a15925569a2d81b010504a3ebde6f050a076.js
@@ -1,4 +1,4 @@
-'use strict';(function(){const input=document.querySelector('#gdoc-search-input');const results=document.querySelector('#gdoc-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/js/flexsearch-ad47a5e1ee.min.js');loadScript('/js/en.search-data.min.b688ac8f102e8692abfe77549c066b3b7128dc79343085e26a2471a33d19ae00.js',function(){input.required=false;search();});}
+'use strict';(function(){const input=document.querySelector('#gdoc-search-input');const results=document.querySelector('#gdoc-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/js/flexsearch-ad47a5e1ee.min.js');loadScript('/js/en.search-data.min.08fc1cf874ce53dc871fc3ebeabe734cfeae8629420c005839b6f8be0b7d98b8.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){console.log("empty")
 results.classList.remove("has-hits");return;}
